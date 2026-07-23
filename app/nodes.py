bfa0e71b329c60d058546df7be36f417ff62c6b0
hyeokjae-frontend-ai <yhdbcontact@gmail.com>
2026-07-23 15:13:46 +0900

    node 기본 구성

---
+++ app/nodes.py
@@ -0,0 +1,24 @@
+from app.state import AgentState
+
+def auth_node(state: AgentState) -> AgentState:
+    pass
+
+
+def router_node(state: AgentState) -> AgentState:
+    pass
+
+
+def inspection_action_agent_node(state: AgentState) -> AgentState:
+    pass
+
+
+def education_agent_node(state: AgentState) -> AgentState:
+    pass
+
+
+def history_agent_node(state: AgentState) -> AgentState:
+    pass
+
+
+def answer_agent_node(state: AgentState) -> AgentState:
+    pass
